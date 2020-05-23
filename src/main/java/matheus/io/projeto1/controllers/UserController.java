890a7617d4d34a61b3f30cf3756fef724325c483
mathe <mathe@DESKTOP-5U7671P>
2020-05-23 12:05:51 -0300

    update on the project with dependency injection (@Autowired) and with a service layer (For control rules)

--- src/main/java/matheus/io/projeto1/controllers/UserController.java
+++ src/main/java/matheus/io/projeto1/controllers/UserController.java
@@ -1,28 +1,22 @@
 package matheus.io.projeto1.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import matheus.io.projeto1.repository.UserRepository;
+import matheus.io.projeto1.service.UserService;
 
 @Controller
 public class UserController {
 
-	private UserRepository userRepository;
-	
-	public UserController(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
+	@Autowired
+	private UserService userService;
 	
 	@RequestMapping("/user")
 	public String getUsers(Model model) {
 		
-		System.out.println("Test");
-		
-		model.addAttribute("usersList", this.userRepository.findAll());
-		
-		System.out.println("Test2");
+		model.addAttribute("usersList", this.userService.findAll());
 		
 		return "user";
 		
